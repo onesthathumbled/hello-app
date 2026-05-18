5b51bf6f458433a05171fc953f5aa818ef46a327
Jai Madera <onesthathumbled@gmail.com>
2026-05-18 13:25:55 +0800

    deploy v5

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from GKE! This is the new version");
+  res.send("Hello from GKE! This is the new version. Test it again.");
 });
 
 app.listen(3000, () => {
